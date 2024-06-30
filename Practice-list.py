96400940abe20a04d72ac99d7cc8f30b3998ad64
rimi <singhrimiumesh@gmail.com>
2024-06-30 13:21:24 +0530

    list done

---
+++ Practice-list.py
@@ -0,0 +1,20 @@
+# fruits = []
+# for i in range(7):
+#     fruits.append(input(f"Enter fruit {i+1}: "))
+# print(fruits)
+
+
+
+# accept marks of 6 students and sort them
+# marks = []
+# for i in range(6):
+#     marks.append(int(input(f"Enter marks for student {i+1}: ")))
+# marks.sort()
+# print(f"Sorted marks: {marks}")
+
+
+
+
+# count number of 0 in a tuple
+t = (1, 3, 0, 4, 0, 0, 2, 0)
+print(t.count(0))
